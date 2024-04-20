5c74b3816e4db54c70c534cb1c8df0f587283982
irisawa <irisawa@ds.it.aoyama.ac.jp>
2024-04-20 13:51:37 +0900

    [ADD] イニングごとの得点

--- src/calculate.py
+++ src/calculate.py
@@ -15,7 +15,7 @@ def win_or_lose(row, team):
             return "△"
 
 
-def calc_goal_diff(row, team):
+def calc_points_diff(row, team):
     if row["team_name_top"] == team:
         return row["points_top"] - row["points_bottom"]
     else:
@@ -27,3 +27,25 @@ def calc_points_losts(row, team):
         return row["points_top"], row["points_bottom"]
     else:
         return row["points_bottom"], row["points_top"]
+
+
+def calc_inning_points(row, team):
+    tmp_row = row.copy()
+    if row["team_name_top"] == team:
+        for i in range(1, 10):
+            tmp_row.rename({f"{i}_top": f"{i}_points"}, inplace=True)
+    else:
+        for i in range(1, 10):
+            tmp_row.rename({f"{i}_bottom": f"{i}_points"}, inplace=True)
+    return tmp_row[[f"{i}_points" for i in range(1, 10)]]
+
+
+def calc_inning_losts(row, team):
+    tmp_row = row.copy()
+    if row["team_name_top"] == team:
+        for i in range(1, 10):
+            tmp_row.rename({f"{i}_bottom": f"{i}_losts"}, inplace=True)
+    else:
+        for i in range(1, 10):
+            tmp_row.rename({f"{i}_top": f"{i}_losts"}, inplace=True)
+    return tmp_row[[f"{i}_losts" for i in range(1, 10)]]
